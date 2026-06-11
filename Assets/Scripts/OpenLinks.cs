e9a1c5a897ee982765635c8228bd971499bac9b2
Yompol-Lab <juanpabloabraham23gmail.com>
2026-06-11 19:52:52 -0300

    Actualizacion link

--- Assets/Scripts/OpenLinks.cs
+++ Assets/Scripts/OpenLinks.cs
@@ -4,6 +4,6 @@ public class OpenLinks : MonoBehaviour
 {
    public void OpenLink()
     {
-        Application.OpenURL("https://discord.gg/AwxGTXgnNy");
+        Application.OpenURL("https://discord.gg/sDy2H2x2aU");
     }
 }
